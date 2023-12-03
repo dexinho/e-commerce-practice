ef1034fd2c638529d4d84da9faa70799fd704a75
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-03 20:56:42 +0100

    added product form listener

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const h1 = document.querySelector('h1')
+const productForm = document.querySelector('#product-form')
+const uploadImageBtn = document.querySelector('#upload-image-btn')
+const productImageInput = document.querySelector('#product-image-input')
 
-h1.style.color = 'red'
+productForm.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const formData = new FormData(e.target)
+  console.log(formData)
+  try {
+    const response = await fetch('/addProduct', {
+      method: 'POST',
+      body: formData
+    })
+
+    if (response.ok) {
+      console.log('Product added!')
+    } else {
+      console.log('Failed to add product')
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+uploadImageBtn.addEventListener('click', () => {
+  productImageInput.click()
+  productImageInput.onchange = (e) => {
+    console.log(e.target)
+  }
+})
