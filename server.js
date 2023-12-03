6291815fe5852b8c1da25835516c408dd26e6128
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-03 20:57:01 +0100

    added form request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fsp = require("fs").promises;
 const path = require("path");
+const { parse } = require("querystring");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -16,12 +17,23 @@ const server = http.createServer(async (req, res) => {
       ".js": "text/javascript",
     }[extname] || "application/octet-stream";
 
-  console.log(contentType);
-
-  res.statusCode = 200;
-  res.setHeader = ("Content-Type", contentType);
-  const data = await fsp.readFile(path.join(__dirname, urlPath));
-  res.end(data, "utf-8");
+  if (req.method === "GET") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", contentType);
+    const data = await fsp.readFile(path.join(__dirname, urlPath));
+    res.end(data, "utf-8");
+  } else if (req.method === "POST") {
+    let body = "";
+    req.on("data", async () => {
+      const formData = parse(body);
+      console.log("Form data received", formData);
+      res.writeHead(302, { Location: "/" });
+      res.end();
+    });
+  } else {
+    res.statusCode = 405;
+    res.end("Method not allowed");
+  }
 });
 
 server.listen(port, hostname, () => {
