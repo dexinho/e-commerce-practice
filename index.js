6bb620319451b0c01f847a02c754d829c7b0dda1
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-04 04:59:32 +0100

    added form on the server side

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
+const formContainer = document.querySelector("#form-container");
 const productForm = document.querySelector("#product-form");
 const uploadImageBtn = document.querySelector("#upload-image-btn");
 const productImageInput = document.querySelector("#product-image-input");
+const deleteConfirmationDialog = document.querySelector(
+  "#delete-confirmation-dialog"
+);
+const deleteConfirmationBtns = document.querySelectorAll(
+  ".delete-confirmation-btns"
+);
+const addNewProductBtn = document.querySelector("#add-new-product-btn");
 const productsContainer = document.createElement("div");
-productsContainer.id = "products-container";
+const productDisplayDiv = document.createElement("div");
 
-const PRODUCTS = [
-  {
-    name: "Bicycle",
-    price: 250,
-    description: "Nice black bicycle",
-    image: "./assets/images/black_bicycle.jpg",
-  },
-  {
-    name: "Backpack",
-    price: 150,
-    description: "Amazing gray backpack",
-    image: "./assets/images/gray_backpack.jpg",
-  },
-  {
-    name: "Headphones",
-    price: 50,
-    description: "Wonderful black headphones",
-    image: "./assets/images/black_headphones.jpg",
-  },
-];
+productDisplayDiv.id = "product-display-div";
+document.body.append(productsContainer);
+
+productsContainer.append(productDisplayDiv);
+
+const addProductListener = (_addNewProductBtn) => {
+  _addNewProductBtn.addEventListener("click", () => {
+    productsContainer.style.display = "none";
+    formContainer.style.display = "flex";
+  });
+};
 
 productForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
-  console.log(formData);
+
   try {
     const response = await fetch("/addProduct", {
       method: "POST",
       body: formData,
     });
+
+    productsContainer.style.display = "flex";
+    formContainer.style.display = "none";
 
     if (response.ok) {
       console.log("Product added!");
@@ -47,20 +49,122 @@ productForm.addEventListener("submit", async (e) => {
 
 uploadImageBtn.addEventListener("click", () => {
   productImageInput.click();
+
   productImageInput.onchange = (e) => {
-    console.log(e.target);
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      console.log("Selected file:", selectedFile);
+    } else {
+      console.log("No file selected");
+    }
   };
 });
 
-const displayProducts = (products) => {
-  const productDisplayDiv = document.createElement("div");
-  const eCommerceTitle = document.createElement("div");
+const removeProduct = async (id) => {
+  const response = await fetch(`/removeProduct?productID=${id}`);
+  const updatedProducts = await response.json();
 
-  productDisplayDiv.id = "product-display-div";
-  eCommerceTitle.id = "e-commerce-title";
+  displayProducts(updatedProducts);
+  deleteConfirmationDialog.close();
+};
+
+const trashListener = (faTrashCan) => {
+  faTrashCan.addEventListener("click", () => {
+    deleteConfirmationDialog.showModal();
+
+    const confirmationListener = (event) => {
+      if (event.target.textContent === "YES") {
+        removeProduct(faTrashCan.id);
+      }
+      deleteConfirmationDialog.close();
+
+      deleteConfirmationBtns.forEach((button) =>
+        button.removeEventListener("click", confirmationListener)
+      );
+    };
+
+    deleteConfirmationBtns.forEach((button) =>
+      button.addEventListener("click", confirmationListener, { once: true })
+    );
+  });
+};
+
+const sortBySelectListener = (_sortBySelect) => {
+  _sortBySelect.addEventListener("change", async () => {
+    if (_sortBySelect === "Sort by") return;
+
+    const response = await fetch(`/getProducts?sortBy=${_sortBySelect.value}`);
+    const updatedProducts = await response.json();
+
+    displayProducts(updatedProducts);
+  });
+};
+
+const searchBarListener = (_searchBarInput) => {
+  _searchBarInput.addEventListener("keypress", async (e) => {
+    if (e.key === "Enter") {
+      const response = await fetch(
+        `/getProducts?search=${_searchBarInput.value}`
+      );
+      const filteredProducts = await response.json();
+      displayProducts(filteredProducts);
+    }
+  });
+};
+
+const displayUserTools = () => {
+  const eCommerceTitle = document.createElement("div");
+  const sortBySelect = document.createElement("select");
+  const addNewProductBtn = document.createElement("button");
+  const sortBy = document.createElement("option");
+  const priceHigh = document.createElement("option");
+  const priceLow = document.createElement("option");
+  const searchBarDiv = document.createElement("div");
+  const searchBarInput = document.createElement("input");
+  const sortAndAddDiv = document.createElement("div");
+
+  sortBy.value = "Sort by";
+  priceHigh.value = "Price high";
+  priceLow.value = "Price low";
+  sortBy.textContent = "Sort by";
+  priceHigh.textContent = "Price high";
+  priceLow.textContent = "Price low";
+  addNewProductBtn.textContent = "ADD PRODUCT";
   eCommerceTitle.textContent = "E-COMMERCE";
 
-  document.body.append(productsContainer);
+  productsContainer.id = "products-container";
+  sortAndAddDiv.id = "sort-and-add-div";
+  sortBySelect.id = "sort-by-select";
+  addNewProductBtn.id = "add-new-product-btn";
+  eCommerceTitle.id = "e-commerce-title";
+  searchBarDiv.id = "search-bar-div";
+  searchBarInput.id = "search-bar-input";
+  searchBarInput.placeholder = "search";
+
+  productsContainer.prepend(eCommerceTitle, sortAndAddDiv, searchBarDiv);
+  searchBarDiv.append(searchBarInput);
+  sortAndAddDiv.append(sortBySelect);
+  sortAndAddDiv.append(addNewProductBtn);
+  sortBySelect.append(sortBy);
+  sortBySelect.append(priceHigh);
+  sortBySelect.append(priceLow);
+
+  sortBySelectListener(sortBySelect);
+  searchBarListener(searchBarInput);
+  addProductListener(addNewProductBtn);
+};
+
+displayUserTools();
+
+const displayProducts = (products) => {
+  productDisplayDiv.textContent = "";
+
+  if (products.length === 0) {
+    const noProducts = document.createElement("div");
+    noProducts.textContent = "No products to show.";
+    productDisplayDiv.append(noProducts);
+    return;
+  }
 
   products.forEach((product) => {
     const productSlot = document.createElement("div");
@@ -73,7 +177,7 @@ const displayProducts = (products) => {
     const faTrashCan = document.createElement("i");
 
     productNameDiv.textContent = product.name;
-    productPriceDiv.textContent = product.price;
+    productPriceDiv.textContent = product.price + "$";
     productDescriptionDiv.textContent = product.description;
     productImage.src = product.image;
 
@@ -84,8 +188,9 @@ const displayProducts = (products) => {
     productImageDiv.className = "product-details product-image-div";
     productDescriptionDiv.className = "product-details product-description-div";
     faTrashCan.className = "fa-solid fa-trash-can";
+    faTrashCan.id = product.id;
+    trashListener(faTrashCan);
 
-    productsContainer.append(eCommerceTitle, productDisplayDiv);
     productDisplayDiv.append(productSlot);
     productSlot.append(
       removeItem,
@@ -99,4 +204,11 @@ const displayProducts = (products) => {
   });
 };
 
-displayProducts(PRODUCTS);
+async function getProducts() {
+  const response = await fetch(`./getProducts`);
+  const products = await response.json();
+
+  displayProducts(products);
+}
+
+getProducts();
--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const http = require("http");
 const fsp = require("fs").promises;
 const path = require("path");
 const { parse } = require("querystring");
+const querystring = require("querystring");
+const multer = require("multer");
 
 const hostname = "127.0.0.1";
 const port = 3000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const server = http.createServer(async (req, res) => {
   const urlPath = req.url === "/" ? "index.html" : req.url;
@@ -17,22 +30,140 @@ const server = http.createServer(async (req, res) => {
       ".js": "text/javascript",
     }[extname] || "application/octet-stream";
 
-  if (req.method === "GET") {
+  if (req.url === "/favicon.ico") {
+    res.writeHead(204, { "Content-Type": "image/x-icon" });
+    res.end();
+    return;
+  }
+
+  if (req.method === "GET" && req.url.startsWith("/removeProduct")) {
+    const urlParts = req.url.split("?");
+    const queryParams = new URLSearchParams(urlParts[1]);
+    const productID = Number(queryParams.get("productID"));
+
+    try {
+      const data = await fsp.readFile("./products.json", "utf-8");
+      let products = JSON.parse(data);
+
+      const i = products.findIndex((product) => product.id === productID);
+      products.splice(i, 1);
+
+      fsp.writeFile("./products.json", JSON.stringify(products), "utf-8");
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(products));
+    } catch (err) {
+      console.log(err);
+    }
+
+    return;
+  }
+
+  if (req.method === "GET" && req.url.startsWith("/getProducts")) {
+    const urlParts = req.url.split("?");
+    const queryParams = new URLSearchParams(urlParts[1]);
+    const sortBy = queryParams.get("sortBy");
+    const searchQuery = queryParams.get("search");
+
+    try {
+      const data = await fsp.readFile("./products.json", "utf-8");
+      let products = JSON.parse(data);
+
+      if (searchQuery) {
+        products = products.filter(
+          (product) => product.name.toLowerCase() === searchQuery.toLowerCase()
+        );
+      }
+
+      if (sortBy === "Price high") {
+        products.sort((a, b) => b.price - a.price);
+      } else if (sortBy === "Price low") {
+        products.sort((a, b) => a.price - b.price);
+      }
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(products));
+    } catch (error) {
+      console.error("Error reading products file:", error.message);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    }
+    return;
+  }
+
+  if (req.method === "POST" && req.url === "/addProduct") {
+    upload.single("product-image-input")(req, res, async (err) => {
+      if (err) {
+        console.error("Error handling file upload:", err.message);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+
+      let body = "";
+
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+
+      req.on("end", async () => {
+        const formData = parse(body);
+
+        const newProduct = {
+          name: formData["product-name-input"],
+          price: formData["product-price-input"],
+          description: formData["product-description-input"],
+        };
+
+        if (req.file) {
+          const { path: tempPath, filename } = req.file;
+          const uploadPath = path.join(__dirname, "uploads", filename);
+
+          await fsp.rename(tempPath, uploadPath);
+
+          newProduct.image = `/uploads/${filename}`;
+        }
+
+        try {
+          const data = await fsp.readFile("./products.json", "utf-8");
+          const products = JSON.parse(data);
+
+          newProduct.id = Date.now();
+
+          products.push(newProduct);
+
+          await fsp.writeFile(
+            "./products.json",
+            JSON.stringify(products, null, 2),
+            "utf-8"
+          );
+
+          res.writeHead(302, { Location: "/" });
+          res.end();
+        } catch (error) {
+          console.error("Error updating products file:", error.message);
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+        }
+      });
+    });
+  } else if (req.method === "GET" && req.url === "/getProducts") {
+    try {
+      const data = await fsp.readFile("./products.json", "utf-8");
+      const products = JSON.parse(data);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(products));
+    } catch (error) {
+      console.error("Error reading products file:", error.message);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    }
+  } else {
     res.statusCode = 200;
     res.setHeader("Content-Type", contentType);
     const data = await fsp.readFile(path.join(__dirname, urlPath));
     res.end(data, "utf-8");
-  } else if (req.method === "POST") {
-    let body = "";
-    req.on("data", async () => {
-      const formData = parse(body);
-      console.log("Form data received", formData);
-      res.writeHead(302, { Location: "/" });
-      res.end();
-    });
-  } else {
-    res.statusCode = 405;
-    res.end("Method not allowed");
   }
 });
 
